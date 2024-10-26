8254d7ca5a79a94d8da3110609ecd5fe492d4a0c
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2024-10-26 12:51:21 +0200

    add Student class

---
+++ src/main/java/org/studentsManager/src/Student.java
@@ -0,0 +1,32 @@
+package org.studentsManager.src;
+
+public class Student {
+    private String name;
+    private int age;
+    private double grade;
+    private String studentID;
+
+    public Student(String name, int age, double grade, String studentID ){
+        this.name = name;
+        this. age = age;
+        this.grade = grade;
+        this.studentID = studentID;
+    }
+    // getters and setters
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public int getAge() { return age; }
+    public void setAge(int age) { this.age = age; }
+
+    public double getGrade() { return grade; }
+    public void setGrade(double grade) { this.grade = grade; }
+
+    public String getStudentID() { return studentID; }
+    public void setStudentID(String studentID) { this.studentID = studentID; }
+
+    public void displayInfo(){
+        System.out.println("ID: " + studentID + ", Name: " + name + ", Age: " + age + ", Grade: " + grade);
+    }
+
+}
